dd025505d4e375de7625bc8f306f64b79ac53cf5
AK <khanna.a@husky.neu.edu>
2019-08-29 01:26:14 -0400

    Deleted Unwanted files

--- project_EventManagement/client/views/event/uploadEvent.controller.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    angular
-        .module("EventApp")
-        .controller("UploadEventController", UploadEventController)
-
-    function UploadEventController(){
-        console.log("Inside UploadEventController");
-    }
-})();
--- project_EventManagement/server/app.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app, db, mongoose) {
-    var userModel = require("./models/user.model.server.js")(db, mongoose);
-    var eventModel = require("./models/event.model.server.js")(db, mongoose);
-
-    var userService = require("./services/user.service.server.js") (app, userModel);
-    var eventService = require("./services/event.service.server.js") (app, userModel, eventModel);
-};
